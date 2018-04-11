e92900e983069b1ae684d90495834fd42730f618
david mimramous <david@example.com>
2018-04-11 22:55:12 +0300

    11.4.2018 - dvir fix the thubms and now its work... dvir is the king

--- app/src/main/java/ind/david/finalproject/Post.java
+++ app/src/main/java/ind/david/finalproject/Post.java
@@ -1,83 +1,87 @@
 package ind.david.finalproject;
 
+import java.util.HashMap;
+
 /**
  * Created by mac on 3/18/18.
  */
 
-public class Post
-{
-    public String date,description,full_name,postimages,profileImage,time,uid;
-
+public class Post {
+    public String date, description, fullName, postimage, profileImage, time, uid;
     public long numOfLikes;
+    public HashMap<String, Boolean> iconStore;
 
-    public Post() {}
 
-    public Post(String date, String description, String fullname, String postimage, String profileImage, String time, String uid,long numOfLikes) {
+
+    public Post () {
+    }
+
+    public Post (String date, String description, String fullName, String postimage, String profileImage, String time, String uid, long numOfLikes, HashMap<String, Boolean> iconStore) {
         this.date = date;
         this.description = description;
-        this.full_name = fullname;
-        this.postimages = postimage;
+        this.fullName = fullName;
+        this.postimage = postimage;
         this.profileImage = profileImage;
         this.time = time;
         this.uid = uid;
         this.numOfLikes = numOfLikes;
+        this.iconStore = iconStore;
     }
 
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    public String getDate() {
+    public String getDate () {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate (String date) {
         this.date = date;
     }
 
-    public String getDescription() {
+    public String getDescription () {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription (String description) {
         this.description = description;
     }
 
-    public String getFullname() {
-        return full_name;
+    public String getFullName () {
+        return fullName;
     }
 
-    public void setFullname(String fullname) {
-        this.full_name = fullname;
+    public void setFullName (String fullName) {
+        this.fullName = fullName;
     }
 
-    public String getPostimage() {
-        return postimages;
+    public String getPostimage () {
+        return postimage;
     }
 
-    public void setPostimage(String postimage) {
-        this.postimages = postimage;
+    public void setPostimage (String postimage) {
+        this.postimage = postimage;
     }
 
-    public String getProfileImage() {
+    public String getProfileImage () {
         return profileImage;
     }
 
-    public void setProfileImage(String profileImage) {
+    public void setProfileImage (String profileImage) {
         this.profileImage = profileImage;
     }
 
-    public String getTime() {
+    public String getTime () {
         return time;
     }
 
-    public void setTime(String time) {
+    public void setTime (String time) {
         this.time = time;
     }
 
-    public String getUid() {
+    public String getUid () {
         return uid;
     }
 
-    public void setUid(String uid) {
+    public void setUid (String uid) {
         this.uid = uid;
     }
 
@@ -89,93 +93,27 @@ public class Post
         this.numOfLikes = numOfLikes;
     }
 
+    public HashMap<String, Boolean> getIconStore () {
+        return iconStore;
+    }
+
+    public void setIconStore (HashMap<String, Boolean> iconStore) {
+        this.iconStore = iconStore;
+    }
+
+
     @Override
     public String toString () {
         return "Post{" +
                 "date='" + date + '\'' +
                 ", description='" + description + '\'' +
-                ", full_name='" + full_name + '\'' +
-                ", postimages='" + postimages + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", postimage='" + postimage + '\'' +
                 ", profileImage='" + profileImage + '\'' +
                 ", time='" + time + '\'' +
                 ", uid='" + uid + '\'' +
                 ", numOfLikes=" + numOfLikes +
+                ", iconStore=" + iconStore +
                 '}';
     }
-
-
-
-    /*    public String uid,time,date,postimage,description ,profileimage,fullname;
-
-
-    public Post(){
-
-
-    }
-
-    public Post(String uid, String time, String date, String postimage, String description, String profileimage, String fullname) {
-        this.uid = uid;
-        this.time = time;
-        this.date = date;
-        this.postimage = postimage;
-        this.description = description;
-        this.profileimage = profileimage;
-        this.fullname = fullname;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getPostimage() {
-        return postimage;
-    }
-
-    public void setPostimage(String postimage) {
-        this.postimage = postimage;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getProfileimage() {
-        return profileimage;
-    }
-
-    public void setProfileimage(String profileimage) {
-        this.profileimage = profileimage;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }*/
 }
